abb976728b2a9276da18265046464ae6340e37d1
HamishFish <hamish.lawrence@studytafensw.edu.au>
2025-05-26 18:25:52 +1000

    added colour to flee enemy

--- Assets/Scripts/State_Machine_Flee.cs
+++ Assets/Scripts/State_Machine_Flee.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class State_Machine_Flee : MonoBehaviour, Trapped
 {
+    private Color flee = Color.cyan;
+    private Color alert = Color.magenta;
+    private Color patrol = Color.green;
+    private Renderer rendition;
     Rigidbody rb;
     float timer = 1;
     public enum States
@@ -14,9 +18,11 @@ public class State_Machine_Flee : MonoBehaviour, Trapped
     public States states;
     public GameObject player;
     Vector3 originScale;
+    public bool isBeingTrapped { get; set; }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rendition = GetComponent<Renderer>();
         rb = GetComponent<Rigidbody>();
         originScale = transform.localScale;
         NextState();
@@ -29,7 +35,7 @@ public class State_Machine_Flee : MonoBehaviour, Trapped
                 StartCoroutine(PatrolState());
                 break;
             case States.Alerted:
-                StartCoroutine(AlertState());
+                StartCoroutine(AlertedState());
                 break;
             case States.Flee:
                 StartCoroutine(FleeState());
@@ -50,6 +56,7 @@ public class State_Machine_Flee : MonoBehaviour, Trapped
         Debug.Log("Entering Patrol State");
         while (states == States.Patrol)
         {
+            rendition.material.color = patrol;
             transform.rotation *= Quaternion.Euler(0f, 50f * Time.deltaTime, 0f);
             if (isFacingGrimm())
             {
@@ -65,7 +72,8 @@ public class State_Machine_Flee : MonoBehaviour, Trapped
         Debug.Log("Entering Alerted State");
         while (states == States.Alerted)
         {
-
+            rendition.material.color = alert;
+            yield return null;
         }
         Debug.Log("Exiting Alerted State");
         NextState();
@@ -75,11 +83,25 @@ public class State_Machine_Flee : MonoBehaviour, Trapped
         Debug.Log("Entering Flee State");
         while(states == States.Flee)
         {
+            rendition.material.color = flee;
             float wave = Mathf.Sin(Time.time * 20f) * 0.1f + 1f;
             float wave2 = Mathf.Cos(Time.time * 20f) * 0.1f + 1f;
             transform.localScale = new Vector3(wave, wave2, wave);
             Vector3 direction = player.transform.position + transform.position;
             rb.AddForce(direction.normalized * (800f * Time.deltaTime));
+            yield return null;
         }
+    }
+    public bool CaptureAnim()
+    {
+        isBeingTrapped = true;
+        timer -= Time.deltaTime * 1f;
+        transform.localScale = Vector3.Lerp(Vector3.zero, originScale, timer);
+        if (timer <= 0) { return false; }
+        return true;
+    }
+    public int Points()
+    {
+        return 3;
     }
 }
